b70eab4bf061aea9bd047edb1892a75296f9a127
billyae <euladdggccyy@gmail.com>
2024-11-02 17:47:04 -0400

    Two hands

--- air_piano.py
+++ air_piano.py
@@ -8,7 +8,7 @@ import time  # 添加此行
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
-detector = HandDetector(detectionCon=0.8)
+detector = HandDetector(detectionCon=0.8,maxHands=2)
 
 # 初始化Pyglet窗口
 keys = ["C", "D", "E", "F", "G", "A", "B"]
@@ -57,31 +57,32 @@ while True:
     hands, img = detector.findHands(img, draw=True)
     if hands:
         # 获取手的关键点
-        hand = hands[0]
-        lmList = hand["lmList"]
-        
-        # Define fingertip indices to track
-        fingertip_indices = [4, 8, 12, 16, 20]  # Thumb, index, middle, ring, and pinky
+        for hand in hands:
 
-        # Check each fingertip position
-        for finger_index, fingertip in enumerate(fingertip_indices):
-            if lmList:
-                finger_x, finger_y = lmList[fingertip][0], lmList[fingertip][1]
+            lmList = hand["lmList"]
+            
+            # Define fingertip indices to track
+            fingertip_indices = [4, 8, 12, 16, 20]  # Thumb, index, middle, ring, and pinky
 
-                # Check if fingertip is touching any key
-                for i, key in enumerate(keys):
-                    x = i * key_width
-                    if x < finger_x < x + key_width and 0 < finger_y < key_height:
-                        keys_being_pressed[key] = True
+            # Check each fingertip position
+            for finger_index, fingertip in enumerate(fingertip_indices):
+                if lmList:
+                    finger_x, finger_y = lmList[fingertip][0], lmList[fingertip][1]
 
-                        # Check if the finger is not already pressing this key
-                        if not key_press_states[key]:
-                            play_key(key)
+                    # Check if fingertip is touching any key
+                    for i, key in enumerate(keys):
+                        x = i * key_width
+                        if x < finger_x < x + key_width and 0 < finger_y < key_height:
+                            keys_being_pressed[key] = True
 
-                            
-                        # Highlight the pressed key
-                        cv2.rectangle(img, (x, 0), (x + key_width, key_height), (0, 255, 0), -1)
-                        cv2.putText(img, key, (x + 30, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+                            # Check if the finger is not already pressing this key
+                            if not key_press_states[key]:
+                                play_key(key)
+
+                                
+                            # Highlight the pressed key
+                            cv2.rectangle(img, (x, 0), (x + key_width, key_height), (0, 255, 0), -1)
+                            cv2.putText(img, key, (x + 30, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
     for key in keys:
         if not keys_being_pressed[key]:
